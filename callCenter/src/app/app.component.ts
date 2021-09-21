fc1aa2e538ef0d6096390e71f476144505c54b46
Prasad Guddad <prasadguddad11@gmail.com>
2021-09-21 13:07:08 +0000

    again

--- callCenter/src/app/app.component.ts
+++ callCenter/src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
-
+import { Component ,OnInit} from '@angular/core';
+declare const M:any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'callCenter';
+export class AppComponent implements OnInit {
+  title = 'Blubirch Appario';
+  ngOnInit(){
+
+  }
+  ngAfterViewChecked(){
+    M.AutoInit();
+    
+  }
+
+
 }
